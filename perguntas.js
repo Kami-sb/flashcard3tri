a8be8943d95f9e2932af0661cf94ed51c529fadc
Kami <103331209+Kami-sb@users.noreply.github.com>
2024-09-30 08:47:59 -0300

    ggrhdrbvih

--- perguntas.js
+++ perguntas.js
@@ -9,163 +9,102 @@
     'Quais eram os povos da mesopotâmia',
     'Acádios, Sumérios, Assírios, Babilônios e Persas ')
 
-    criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
+
     criaCartao(
     'História',
     'Data da 1° guerra mundial ',
     '1914 a 1918')
 
-    criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
+  
     criaCartao(
     'História',
     'Data da revolução francesa',
     '1789')
 
-    criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
+   
     criaCartao(
     'Física',
     'Fórmula da tensão elétrica? ',
     'U=R×i')
     
-    criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
+  
     criaCartao(
     'Física',
     'Qual a unidade de medida da intensidade de corrente?',
     'Amper')
     
     criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
-    criaCartao(
     'História',
     'Em que ano se deu o golpe militar brasileiro',
     '1964')
 
     criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
-    criaCartao(
     'Química',
     'Qual a fórmula da concentração de uma substância ',
     'C= massa do soluto/volume da solução')
 
-    criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
+   
     criaCartao(
     'História',
     'Qual o regime defendido pelos EUA',
     'Capitalismo')
 
-    criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
+   
     criaCartao(
     'Matemática',
     'Qual a fórmula do trapézio',
     'B+b×h/2')
-    criaCartao(
-    'Categoria',
     
-    'Pergunta',
-    'Resposta')
     criaCartao(
     'Química',
     'Fórmula da densidade',
     'D=massa da solução/volume da solução')
 
     criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
-    criaCartao(
     'Química',
     'Fórmula do Título',
     'T=massa do soluto/massa do soluto+massa do solvente')
 
     criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
-    criaCartao(
     'Química',
     'Fórmula da Molaridade',
     'M= n° mol soluto/volume da solução')
 
-    criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
+   
     criaCartao(
     'Química',
     'Número da constante de avogadro ',
     '6,022×10^23')
 
     criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
-    criaCartao(
     'Matemática',
     'Qual a relação de euler',
     'V+F=A+2')
 
     criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
-    criaCartao(
     'Biologia',
     'O tecido sangüíneo apresenta...',
     'Plaquetas, leucócitos e hemácias')
 
-    criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
+
     criaCartao(
     'Biologia',
     'O que é a sístole',
     'É a contração do músculo cardíaco')
 
-    criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
+   
     criaCartao(
     'Física',
     'Fórmula do calor latente',
     'Q=m×L')
 
-    criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
+   
     criaCartao(
     'Física',
     'Formula do calor específico',
     'Q=m×c×t')
 
-    criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta')
+ 
     criaCartao(
     'Física',
     'Qual a equação do espaço no movimento uniforme',
