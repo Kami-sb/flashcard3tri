742cd29d4e086e6f99ded3cd7e53b70e07d6520f
Kami <103331209+Kami-sb@users.noreply.github.com>
2024-09-30 08:12:16 -0300

    hg

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let container = document.getElementById('container');
 let cartao = document.createElement('article')
 cartao.clasName = 'cartao'
 cartao.innerHTML = `
-<div> class="conteudo-cartao"
+<div class="conteudo-cartao"
     <h3>${categoria}</h3>
 <div class="pergunta-cartao">
 <p>${pergunta}</p>
 </div>
-    <div>class="resposta-cartao" 
+    <div class="resposta-cartao" 
 <p>${resposta}</p>
     </div>
 </div>
